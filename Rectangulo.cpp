a2f69af360707787adc9cde7b0e81f89bf6a9f08
RubenMoralesSavi <50713874+RubenMoralesSavi@users.noreply.github.com>
2019-06-05 18:29:46 -0500

    Add files via upload

---
+++ Rectangulo.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <pch.h>
+
+int Rectangulo::Rectangulo(double h, double l) {
+	class Rectangulo {
+		double altura_;
+		double ancho_;
+	public :
+		Rectangulo(double, double);
+		double Area();
+	}
+	ancho_ = l;
+	alto_ = h;
+
+double Rectangulo::Area();
+return alto * ancho;
+}
+
+int main() {
+	Rectangulo figura(3, 4);
+	cout << "El area es : " << figura.area() << endl;
+	Rectangulo otrafig();
+	cout << "El area es : " << otrafig.area() << endl;
+}
+Rectangulo();
+Rectangulo::Rectangulo() {
+	ancho = 5;
+	alto = 7;	    
+}
